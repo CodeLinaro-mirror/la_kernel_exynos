e2de32e5430be9c39b1300bd6dbcb0ee4cd7407e
Wlogsky <wlogsky@google.com>
2024-02-07 15:39:40 +0000

    display: Add HBM support
    Expanding brightness range for HBM so that the design can fit into
    Android U design.
    
    Bug: 317308706
    Test: adb shell echo 256 > /sys/class/backlight/panel_0/brightness
    Change-Id: I495bf0cc7c1339a1b0e0599985c76c7c46702010
    Signed-off-by: Wlogsky <wlogsky@google.com>

--- drivers/video/fbdev/exynos/dpu30/panels/exynos_panel.h
+++ drivers/video/fbdev/exynos/dpu30/panels/exynos_panel.h
@@ -106,6 +106,12 @@ struct exynos_panel_info {
 	unsigned int width;
 	unsigned int height;
 
+	/* high brightness mode */
+	unsigned int hbm_min_level;
+	unsigned int hbm_max_level;
+	unsigned int transition_point;
+	bool is_hbm_on;
+
 	unsigned int hs_clk;
 	struct stdphy_pms dphy_pms;
 	unsigned int esc_clk;
