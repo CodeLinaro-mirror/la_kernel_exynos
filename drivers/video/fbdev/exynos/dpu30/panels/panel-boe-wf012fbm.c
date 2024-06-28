c69b1f15f72e94a07d879f10928cf62ebb8625e3
Weilin Wu <wlogsky@google.com>
2024-06-28 09:52:09 +0000

    display: Do not ignore the br request in DOZE mode
    This was originally implemented as a workaround for an issue with master
    DBV control. With the introduction of separate DBV regs for each display
    mode, this workaround is no longer necessary.
    
    Bug: 347773213
    Test: Switching between NBM & AoD
    Change-Id: Ic45e01796f142d54a5925a652f7349427fe1e477
    Signed-off-by: Weilin Wu <wlogsky@google.com>

--- drivers/video/fbdev/exynos/dpu30/panels/panel-boe-wf012fbm.c
+++ drivers/video/fbdev/exynos/dpu30/panels/panel-boe-wf012fbm.c
@@ -411,20 +411,6 @@ static int wf012fbm_set_light(struct exynos_panel_device *panel, u32 br_val)
 	u8 aod_brightness;
 	u8 hbm_brightness;
 	struct dsim_device *dsim = get_dsim_drvdata(0);
-	/*
-	 * Only set brightness if it's not currently in doze mode as AP
-	 * assumes DBVs are in 650 nits range while in fact it could be
-	 * in 150 nits range if the device already entered doze.
-	 *
-	 * In order to prevent wrong brightness set due to this 650/150
-	 * nits scaling, the MCU will be solely responsible for controlling
-	 * display brightness during doze (150 nits range). Thus, this
-	 * function would do nothing if the device is currently in doze.
-	 */
-	if (dsim->state == DSIM_STATE_DOZE) {
-		DPU_INFO_PANEL("%s: skip doze br=%d\n", __func__, br_val);
-		return 0;
-	}
 
 	mutex_lock(&panel->ops_lock);
 
